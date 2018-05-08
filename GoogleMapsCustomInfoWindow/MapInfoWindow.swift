e52f74fb2feeafdb41d2ca8a008e99d112ad575c
Meghana Trivedi <36062849+meghanatrivedi97222@users.noreply.github.com>
2018-05-08 10:49:57 +0530

    Add files via upload

---
+++ GoogleMapsCustomInfoWindow/MapInfoWindow.swift
@@ -0,0 +1,27 @@
+//
+//  MapInfoWindow.swift
+//  GoogleMapsCustomInfoWindow
+//
+//  Created by Sofía Swidarowicz Andrade on 11/5/17.
+//  Copyright © 2017 Sofía Swidarowicz Andrade. All rights reserved.
+//
+
+import UIKit
+
+class MapInfoWindow: UIView {
+
+    @IBOutlet weak var titleInfo: UILabel!
+    @IBOutlet weak var buttonAction: UIButton!
+
+
+    @IBAction func Test(_ sender: Any) {
+        print("i m ios")
+    }
+    @IBAction func didTapInButton(_ sender: Any) {
+        print("button tapped")
+    }
+    
+    class func instanceFromNib() -> UIView {
+        return UINib(nibName: "MapInfoWindowView", bundle: nil).instantiate(withOwner: self, options: nil).first as! UIView
+    }
+}
